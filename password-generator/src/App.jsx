43587d40333be7a988850b3c566d43f1728f1cd2
simran5555 <simran.kaur@ayasya.com>
2024-04-12 17:00:16 +0530

    progressbar folder added

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [password, setPassword] = useState();
+  const [upper, setUpper] = useState(false)
+  const [lower, setLower] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [length, setLength] = useState(8)
+  const [strength, setStrength] = useState(false)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
+        <div className="flex-between">
+          <span>password</span>
+          <button>Copy</button>
+        </div>
+        <div className="flex-between">
+          <span>character lenghth</span>
+          <span>{length}</span>
+        </div>
+        <input type="range" 
+                style={{ width: "100%" }}
+                min={8}
+                max={25}
+                step={1}
+                onChange={(e)=>setLength(e.target.value)}
+         />
+        <div className="flex-evenly">
+          <div>
+            <div class="form-check mb-3">
+              <input
+                id="numbers"
+                name="numbers"
+                type="checkbox"
+                class="form-check-input"
+                value=""
+              />
+              <label id="numbers_label" class="form-check-label" for="numbers">
+                Include Numbers
+              </label>
+            </div>
+            <div class="form-check mb-3">
+              <input
+                id="symbols"
+                name="symbols"
+                type="checkbox"
+                class="form-check-input"
+                value=""
+              />
+              <label id="symbols_label" class="form-check-label" for="symbols">
+                Include Special Character
+              </label>
+            </div>
+          </div>
+          <div>
+            <div class="form-check mb-3">
+              <input
+                id="uppercase"
+                name="uppercase"
+                type="checkbox"
+                class="form-check-input"
+                value=""
+              />
+              <label
+                id="uppercase_label"
+                class="form-check-label"
+                for="uppercase"
+              >
+                Include Upper Case
+              </label>
+            </div>
+            <div class="form-check mb-3">
+              <input
+                id="lowercase"
+                name="lowercase"
+                type="checkbox"
+                class="form-check-input"
+                value=""
+              />
+              <label
+                id="lowercase_label"
+                class="form-check-label"
+                for="lowercase"
+              >
+                Include Lower Case
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="flex-between">
+          <span>Strength</span>
+          <span>moderate</span>
+        </div>
+        <div className="button">
+          <button>Generate Password</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+);
 }
-
-export default App
+export default App;
