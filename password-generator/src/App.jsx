f1ccd1c2f452dd83474110b97961542c82f7c4b3
simran5555 <simran.kaur@ayasya.com>
2024-04-15 11:13:09 +0530

    password generator done

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,106 +1,89 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/use-password-generator";
+import PasswordStrengthIndicator from "./components/StrengthChecker";
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
 
-function App() {
+export default function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false }
+  ]);
+  const [copied, setCopied] = useState(false);
 
-  const [password, setPassword] = useState();
-  const [upper, setUpper] = useState(false)
-  const [lower, setLower] = useState(false)
-  const [symbols, setSymbols] = useState(false)
-  const [numbers, setNumbers] = useState(false)
-  const [length, setLength] = useState(8)
-  const [strength, setStrength] = useState(false)
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="flex-between">
-          <span>password</span>
-          <button>Copy</button>
+    <div className="container">
+      {/* Password Text and Copy */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <Button
+            text={copied ? "Copied" : "copy"}
+            onClick={handleCopy}
+            customClass="copyBtn"
+          />
         </div>
-        <div className="flex-between">
-          <span>character lenghth</span>
-          <span>{length}</span>
-        </div>
-        <input type="range" 
-                style={{ width: "100%" }}
-                min={8}
-                max={25}
-                step={1}
-                onChange={(e)=>setLength(e.target.value)}
-         />
-        <div className="flex-evenly">
-          <div>
-            <div class="form-check mb-3">
-              <input
-                id="numbers"
-                name="numbers"
-                type="checkbox"
-                class="form-check-input"
-                value=""
-              />
-              <label id="numbers_label" class="form-check-label" for="numbers">
-                Include Numbers
-              </label>
-            </div>
-            <div class="form-check mb-3">
-              <input
-                id="symbols"
-                name="symbols"
-                type="checkbox"
-                class="form-check-input"
-                value=""
-              />
-              <label id="symbols_label" class="form-check-label" for="symbols">
-                Include Special Character
-              </label>
-            </div>
-          </div>
-          <div>
-            <div class="form-check mb-3">
-              <input
-                id="uppercase"
-                name="uppercase"
-                type="checkbox"
-                class="form-check-input"
-                value=""
-              />
-              <label
-                id="uppercase_label"
-                class="form-check-label"
-                for="uppercase"
-              >
-                Include Upper Case
-              </label>
-            </div>
-            <div class="form-check mb-3">
-              <input
-                id="lowercase"
-                name="lowercase"
-                type="checkbox"
-                class="form-check-input"
-                value=""
-              />
-              <label
-                id="lowercase_label"
-                class="form-check-label"
-                for="lowercase"
-              >
-                Include Lower Case
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="flex-between">
-          <span>Strength</span>
-          <span>moderate</span>
-        </div>
-        <div className="button">
-          <button>Generate Password</button>
-        </div>
+      )}
+      {/* Character Length */}
+      <div className="charlength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
       </div>
+      {/* Checkboxes */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <Checkbox
+              key={index}
+              title={checkbox.title}
+              onChange={() => handleCheckboxChange(index)}
+              state={checkbox.state}
+            />
+          );
+        })}
+      </div>
+      {/* Strength */}
+      <PasswordStrengthIndicator password={password} />
+
+      {/* Error Handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+
+      {/* Generate Button */}
+      <Button
+        text="Generate Password"
+        onClick={() => generatePassword(checkboxData, length)}
+        customClass="generateBtn"
+      />
     </div>
-);
+  );
 }
-export default App;
